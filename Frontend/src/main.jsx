2807a8dafd61401eb0827a204203e91585c608c0
Jatin Saxena <saxenajatin108@gmail.com>
2026-06-18 17:55:19 +0530

    updated font family

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,6 +5,10 @@ import AuthProvider from "./context/AuthContext";
 
 import ThemeProvider from "./context/ThemeContext";
 import AppToaster from "./components/AppToaster";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
